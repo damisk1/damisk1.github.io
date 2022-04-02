b80034b9c188c52e23cfe65481dc1ce9757c64e5
damisk1 <gabrieldamiski@hotmail.com>
2022-04-02 17:08:47 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function abrirMenu(){
+    document.getElementById('header_menu_hamburger_btn').classList.toggle('change');
+    document.getElementById('header_menu').classList.toggle('change');
+}
